4c68d7674fbf02d9c402ecefd64e8b78ad738146
Roberto Muñoz <rmunozdev@gmail.com>
2018-08-06 14:07:03 -0500

    Actualizacion de sp sp_registrar_fin_actividad

--- database/stored_procedures/sp_registrar_fin_actividad.sql
+++ database/stored_procedures/sp_registrar_fin_actividad.sql
@@ -1,19 +1,6 @@
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `sp_registrar_fin_actividad` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = cp850 */ ;
-/*!50003 SET character_set_results = cp850 */ ;
-/*!50003 SET collation_connection  = cp850_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_registrar_fin_actividad`(
 	IN _NUM_ACTIV INT(11),
+    IN _MSG_INF_ACTIV VARCHAR(300),
     IN _ERR_TEC_ACTIV VARCHAR(250),
     IN _EST_ACTIV VARCHAR(7)
 )
@@ -22,10 +9,10 @@ BEGIN
 	UPDATE tb_actividad 
     SET
 		fec_fin_ejec_activ = NOW(),
+        msg_inf_activ = _MSG_INF_ACTIV,
         err_tec_activ = _ERR_TEC_ACTIV,
         est_activ = _EST_ACTIV
 	WHERE
 		num_activ = _NUM_ACTIV;
 
-END ;;
-DELIMITER ;
+END
