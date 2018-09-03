0e17449fe074041b00d3e685314404ae0f039b64
Roberto Muñoz <rmunozdev@gmail.com>
2018-09-02 22:49:13 -0500

    Homologación contenido para base de datos

--- database/scripts/QUERY_PROC_ACTIV.sql
+++
@@ -1,2 +0,0 @@
-select * from tb_proceso;
-select * from tb_actividad;
--- database/scripts/UPD_EJEC_PROC_BATCH.sql
+++
@@ -1,9 +0,0 @@
-UPDATE bd_gesateped.tb_parametro par
-SET par.val_param = '20:37'
-WHERE par.cod_param = 1;
-
-COMMIT;
-
-SELECT par.*
-FROM bd_gesateped.tb_parametro par
-WHERE par.cod_param = 1;
